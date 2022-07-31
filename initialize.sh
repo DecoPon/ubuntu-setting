b6def43f28f324df2923c44537d15097a6746e36
DecoPon <indy12_dev@outlook.com>
2022-07-31 12:30:43 +0900

    Fix prezto script

--- initialize.sh
+++ initialize.sh
@@ -1,14 +1,8 @@
 # 必要なパッケージを入れておく
-sudo apt -y update && sudo apt upgrade
+sudo apt -y update && sudo apt -y upgrade
 sudo apt -y install git zsh ssh
-
-# startup filesは "q" で一旦スキップ
-zsh
 
 # preztoを導入
 git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
-setopt EXTENDED_GLOB
-for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
-  ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
-done
+zsh ./prezto.sh
 chsh -s /usr/bin/zsh
